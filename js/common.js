002e2acc0973e0945fd67baf0c961562384660f6
WebIT <WebIT>
2016-11-01 17:39:59 +0200

    01.11.2016

--- js/common.js
+++ js/common.js
@@ -4,6 +4,49 @@ $('.slick-slider').slick({
 $('.clients-slick').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
-    autoplay: true
+    autoplay: true,
+    wariableWidth: true,
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 530,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 430,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
